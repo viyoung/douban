68c9703932bac8988ae756211c14cbb690848e4e
viyoung <971416434@qq.com>
2018-12-08 17:46:05 +0800

    pushName:viyoung; iussue:ssr html

---
+++ server/tpl/normal.js
@@ -0,0 +1,19 @@
+module.exports = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>douban</title>
+</head>
+<body>
+   <div class="container">
+   <div>
+   <h1>hi luke</h1>
+   <p>this is viyoung</p>
+   </div>
+   </div> 
+</body>
+</html>
+`
